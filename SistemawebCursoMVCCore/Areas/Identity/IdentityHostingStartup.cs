77b90417de12313e63a4c7ad59b326b1c9f345ec
Veronica <green.veronica@gmail.com>
2020-02-26 12:55:58 -0300

    Agregado Area Identidad

---
+++ SistemawebCursoMVCCore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemawebCursoMVCCore.Data;
+
+[assembly: HostingStartup(typeof(SistemawebCursoMVCCore.Areas.Identity.IdentityHostingStartup))]
+namespace SistemawebCursoMVCCore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
